6dad6b5fe20a09e361c5129a0d9d418ae49b6400
dorthl <x344527085@outlook.com>
2023-09-14 11:07:10 +0800

    models load

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,11 @@
 # models
-cd data/
+sudo chown -R -v dex:wheel data
+cd data/models/Stable-diffusion
+git lfs install
+git lfs pull
+git clone https://huggingface.co/hakurei/waifu-diffusion-v1-4
+git clone https://huggingface.co/Linaqruf/anything-v3.0
+git clone https://huggingface.co/Lykon/DreamShaper
 
-https://huggingface.co/hakurei/waifu-diffusion-v1-4
+
+docker compose --profile auto up --build
